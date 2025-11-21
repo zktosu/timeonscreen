ba1aee71e6c7111eee9e4b027ff2de4e5d23ed33
zktosu <zktosu@gmail.com>
2025-11-21 20:58:46 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # timeonscreen
 A simple mac tool to show total screen on time (hours) on battery since given charged time.
+
+to use simply change the "txt" time and timezone for yourself.
+
+on terminal run or build then run this code.
---
+++ getusagetime.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func main() {
+	shellCommand := `pmset -g log | grep "Display is turned"`
+	cmd := exec.Command("bash", "-c", shellCommand)
+	o, _ := cmd.Output()
+	// output is []string
+	output := strings.Split(string(o),"\n")
+	// give the AC unplug date and time manually
+	// yeah, but works for me.
+	txt := "2025-11-21 19:52:00 +0300"
+	// this date is spesifically selected by golang developers.
+	// used for date template
+	layout := "2006-01-02 15:04:05 -0700"
+	// a time we got from last charged date log 
+	beginning,_ := time.Parse(layout,txt)
+	// this will keep if screen is on
+	var onScreen = beginning
+	var totalTime float64
+	for _, line := range output{
+		if line == ""{
+			continue
+		}
+		dateLine := strings.TrimSpace(line)
+		curDate,_ := time.Parse(layout,dateLine[:25])
+		// if date earlier than last charge 
+		// discard 
+		if curDate.Before(beginning){
+			continue
+		}
+		if strings.Contains(dateLine," on"){
+			onScreen = curDate
+		}else{
+			if !onScreen.IsZero(){
+				totalTime += curDate.Sub(onScreen).Hours()
+			}
+			onScreen = time.Time{}
+		}
+	}
+	if !onScreen.IsZero(){
+		totalTime += time.Now().Sub(onScreen).Hours()
+	}
+	fmt.Println("Total screen on battery usage:",totalTime," hours")
+}
